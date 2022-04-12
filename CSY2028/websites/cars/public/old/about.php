131a81ceb9026b1cf2ac94b8a4246ff866fc6998
emmanuelnn <77127273+emmanuelnn@users.noreply.github.com>
2022-04-12 03:00:44 +0100

    Add files via upload

---
+++ CSY2028/websites/cars/public/old/about.php
@@ -0,0 +1,10 @@
+<?php
+require '../functions/loadTemplate.php';
+require '../autoload.php';
+$title = 'Claires Cars - Home';
+
+$output = loadTemplate('../templates/about.html.php', []);
+
+
+require  '../templates/layout.html.php';
+
---
+++ CSY2028/websites/cars/public/old/careers.php
@@ -0,0 +1,9 @@
+<?php
+require '../functions/loadTemplate.php';
+require '../autoload.php';
+$title = 'Internet Joke Database';
+
+$output = loadTemplate('../templates/careers.html.php', []);
+
+
+require  '../templates/layout.html.php';
